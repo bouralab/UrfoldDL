6c7ffdf310231f9ee5abed137e63e86fd274395d
Eli Draizen <edraizen@gmail.com>
2019-09-11 12:57:02 -0400

    Create mmtf-lifcycle.sh

---
+++ mmtf-lifcycle.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+conda install -y -n python3 openjdk==8.0.152 pyspark==2.3.2 biopython==1.72 numba==0.41.0
+source activate python3 
+pip install git+https://github.com/sbl-sdsc/mmtf-pyspark.git
